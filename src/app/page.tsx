7926ff3dbc3521d84101d3a29b8939ff2a1870b2
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-06-24 00:48:15 +0300

    feat: `question` template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import Button from "@/components/UI/button/Button";
 import Seaarch from "../components/UI/search/Seaarch";
 import Card from "@/components/UI/card/Card";
+import Question from "@/components/UI/question/Question";
 
 export default function Home() {
   return (
@@ -16,10 +17,16 @@ export default function Home() {
       <Button type={'secondary'} text={'Нет'}/>
       <br/>
       <br/>
+      <Button type={'arrow'}/>
+      <br/>
+      <br/>
       <Seaarch placeholder="Введите название" title='Назание'/> 
       <br/>
       <br/>
       <Card isCart={true} picture={'/card.png'} title={'Властелин колец: Братство кольца'} genre={'Фэнтези'}/>
+      <br/>
+      <br/>
+      <Question type={'declaraion'} title={"Ну вот и все"} text={"Мы — крупнейший сервис о кино в рунете. На нем вы сможете посмотреть фильмы и сериалы, купить билеты в кино, узнать рейтинги популярных видео и интересные факты, поставить фильмам оценки, написать рецензии и дополнить описание фильмов."}/>
     </div>
   );
 };
---
+++ src/components/UI/question/Question.tsx
@@ -0,0 +1,26 @@
+import { FC, useEffect, useState } from 'react';
+import styles from './Question.module.scss'
+import Image from 'next/image'
+
+interface IQuestion {
+	type: string;
+  title: string;
+  text?: string;
+}
+
+const Question: FC<IQuestion> = ({type, title, text}) => {
+  const heading = type === 'declaration' ? styles.declaration : styles.question;
+  const [isOpen, setIsOpen] = useState(()=>{return false});
+  const arrow = isOpen ? styles.active : styles.nonActive;
+  return (
+    <div className={styles.block}>
+      <div className={styles.line}>
+        <h2 className={heading}>{title}</h2>
+        {type !== 'declaration' && <Image onClick={() => {setIsOpen(!isOpen)}} className={arrow} src='/arrow.svg' width={20} height={20} alt="arrow"/>}
+      </div>
+      {isOpen && type !== 'declaration' && <p className={styles.text}>{text}</p>}
+    </div>
+  );
+};
+
+export default Question;
