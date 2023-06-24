25a32f4d7c3c16f58a82a66f84a9769bd0d04364
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-06-24 19:08:46 +0300

    remove: extra `imports` from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,4 @@
 "use client"
-import Button from "@/components/UI/button/Button";
-import Seaarch from "../components/UI/search/Seaarch";
-import Card from "@/components/UI/card/Card";
-import Question from "@/components/UI/question/Question";
-import Revue from "@/components/UI/revue/Revue";
-import MovieCard from "@/components/UI/movieCard/MovieCard";
-import Header from "@/components/header/Header";
-import Footer from "@/components/footer/Footer";
 import Filter from "@/components/filter/Filter";
 import Main from "@/components/main/Main";
 
