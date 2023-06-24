f718f9b30db567c95473672bdf06aac9bd9379f6
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-06-24 13:01:47 +0300

    feat: `MovieCard` component

---
+++ src/components/UI/movieCard/MovieCard.tsx
@@ -0,0 +1,39 @@
+import { FC } from "react";
+import styles from './MovieCard.module.scss';
+import Image from 'next/image'
+
+interface IMovieCard {
+  name: string;
+  genre: string;
+  year: number;
+  director: string;
+  text: string;
+  picture: string;
+  grade: number;
+}
+
+const MovieCard: FC<IMovieCard> = ({name, genre, year, grade, director, text, picture}) => {
+  return (
+    <div className={styles.card}>
+      <Image className={styles.picture} src={picture} width={400} height={500} alt="Picture of the movie"/>
+      <div className={styles.data}>
+        <div className={styles.header}>
+          <h2 className={styles.name}>{name}</h2>
+          <div className={styles.control}>
+            .../.../...
+          </div>
+        </div>
+        <div className={styles.info}>
+          <h3 className={styles.title}>Жанр: <span className={styles.value}>{genre}</span></h3>
+          <h3 className={styles.title}>Год выпуска: <span className={styles.value}>{year}</span></h3>
+          <h3 className={styles.title}>Рейтинг: <span className={styles.value}>{grade}</span></h3>
+          <h3 className={styles.title}>Режиссёр: <span className={styles.value}>{director}</span></h3>
+          <h3 className={styles.title}>Описание</h3>
+          <p className={styles.text}>{text}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
