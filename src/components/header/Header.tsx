07f013c4f5d57f44393c13d150443f44f1773ed3
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-06-26 01:11:02 +0300

    feat: cart `counter`

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from './Header.module.scss';
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
+import { store } from "@/redux/store";
 
 interface IHeader {
   inCart?: number;
@@ -12,11 +13,16 @@ interface IHeader {
 const Header: FC<IHeader> = ({inCart=0}) => {
   const pathname = usePathname();
   const router = useRouter();
+  const [cart, setCart] = useState(0)
+  store.subscribe(()=>{
+    console.log(store.getState())
+    setCart(Object.values(store.getState().cart).reduce((acc, curr) => acc + curr, 0))
+  })
   return (
     <header className={styles.header}>
       <h1 onClick={()=>{pathname !== "/" && router.push("/")}} className={styles.title} >Билетопоиск</h1>
       <div onClick={()=>{pathname !== "/cart" && router.push("/cart")}} className={styles.cart}>
-        {inCart>0 && <div className={styles.value}><p>{inCart}</p></div>}
+        {cart>0 && <div className={styles.value}><p>{cart}</p></div>}
         <Image src={'/cart.svg'} alt="cart" width={32} height={32} />
       </div>
     </header>
