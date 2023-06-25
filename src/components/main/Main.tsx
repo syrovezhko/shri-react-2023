1018d751baa3e7415e5591186a372bb3859d3dbe
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-06-25 17:31:40 +0300

    feat: `Card` generation with API

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -2,20 +2,23 @@
 import { FC } from "react";
 import styles from './Main.module.scss';
 import Card from "../UI/card/Card";
+import { IMovie, getAllMovies } from "@/apiProcessing/getAllMovies";
 
 interface IMain {
 }
 
-const Main: FC<IMain> = () => {
+const Main: FC<IMain> = async () => {
+  const movies = (await getAllMovies()) || [];
   return (
     <main className={styles.main} >
-      <Card picture='/card.png' title="jjjjjjjjjjjjjjjjjj" genre="rrrrrrrrrrrrr" />
-      <Card picture='/card.png' title="jjjjjjjjjjjjjjjjjj" genre="rrrrrrrrrrrrr" />
-      <Card picture='/card.png' title="jjjjjjjjjjjjjjjjjj" genre="rrrrrrrrrrrrr" />
-      <Card picture='/card.png' title="jjjjjjjjjjjjjjjjjj" genre="rrrrrrrrrrrrr" />
-      <Card picture='/card.png' title="jjjjjjjjjjjjjjjjjj" genre="rrrrrrrrrrrrr" />
-      <Card picture='/card.png' title="jjjjjjjjjjjjjjjjjj" genre="rrrrrrrrrrrrr" />
-
+      {movies.map((movie: IMovie) => (
+        <Card
+          title={movie.title}
+          picture={movie.posterUrl}
+          genre={movie.genre}
+          id={movie.id}
+          key={movie.id}/>
+      ))}
     </main>
   );
 };
