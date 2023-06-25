94cc503e6f6ef5bf88535be365e97d5ba2bd0a23
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-06-25 17:25:02 +0300

    feat: links pathes to `Header`

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,21 @@
+"use client"
+
 import { FC } from "react";
 import styles from './Header.module.scss';
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 
 interface IHeader {
   inCart?: number;
 }
 
 const Header: FC<IHeader> = ({inCart=0}) => {
+  const pathname = usePathname();
+  const router = useRouter();
   return (
     <header className={styles.header}>
-      <h1 className={styles.title} >Билетопоиск</h1>
-      <div className={styles.cart}>
+      <h1 onClick={()=>{pathname !== "/" && router.push("/")}} className={styles.title} >Билетопоиск</h1>
+      <div onClick={()=>{pathname !== "/cart" && router.push("/cart")}} className={styles.cart}>
         {inCart>0 && <div className={styles.value}><p>{inCart}</p></div>}
         <Image src={'/cart.svg'} alt="cart" width={32} height={32} />
       </div>
