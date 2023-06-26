74bad4714b891c4a8aca1f40288ca4ad98c19bc1
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-06-26 20:49:07 +0300

    feat: `question` page link

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,14 +2,17 @@
 
 import { FC } from "react";
 import styles from './Footer.module.scss';
+import { usePathname, useRouter } from "next/navigation";
 
 interface IFooter {
 }
 
 const Footer: FC<IFooter> = () => {
+  const pathname = usePathname()
+  const router = useRouter()
   return (
     <footer className={styles.footer}>
-      <h3 className={styles.text}>Вопросы-ответы</h3>
+      <h3 onClick={()=>{pathname !== "/question" && router.push("/question")}} className={styles.text}>Вопросы-ответы</h3>
       <h3 className={styles.text}>О нас</h3>
     </footer>
   );
